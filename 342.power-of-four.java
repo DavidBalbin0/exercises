160d0d25be272a51ec875db21a58e718573d27e1
david.balbino <david.balbino@altvia.com.br>
2025-08-15 20:00:00 -0300

    Implement initial solution for Power of Four problem

---
+++ 342.power-of-four.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=342 lang=java
+ *
+ * [342] Power of Four
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n == 0 || n < 0){
+            return false;
+        }
+
+        if (((n & (n - 1
+        )) == 0) && ((n & 0x55555555) != 0))  {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+// @lc code=end
+
